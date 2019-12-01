d8828c7b464241b13920f34f678138af6c5e0fbb
Yannik Seitz <yannikseitz@outlook.de>
2019-12-01 18:47:56 +0100

    Add project files.

---
+++ Spiel/MainWindow.xaml.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Spiel
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			timer.Interval = TimeSpan.FromSeconds(0.02);
+			timer.Tick += Update;
+		}
+
+		DispatcherTimer timer = new DispatcherTimer();
+		Raumschiff raumschiff = null;
+		List<SpielObjekt> spielObjekte = new List<SpielObjekt>();
+		bool spielLaeuft = false;
+		double zeit;
+		int vergangen;
+		int leben;
+		double score;
+		bool schiessen;
+		int upgrades;
+		int xp;
+		bool alternieren;
+		bool alternieren2;
+
+		void Update(object sender, EventArgs e)
+		{
+			if (spielLaeuft == true)
+			{
+				zeit += 0.2 * 0.2;
+				textBlock_time.Text = zeit.ToString("0.##");
+				textBlock_score.Text = score.ToString("0.");
+				textBlock_health.Text = leben.ToString();
+				prograssBar_health.Value = leben;
+				textBlock_lebenStein.Text = (10 + vergangen).ToString();
+				textBlock_upgrades.Text = upgrades.ToString();
+				textBlock_xp.Text = xp.ToString();
+
+				if (leben > 0)
+				{
+					raumschiff.Rotieren(zeichenflaeche, zeichenflaeche.PointToScreen(Mouse.GetPosition(this)));
+					List<SpielObjekt> abfall = new List<SpielObjekt>();
+
+					foreach (SpielObjekt spielObjekt in spielObjekte)
+					{
+						if (spielObjekt.Animiere(zeichenflaeche, timer.Interval) && spielObjekt is PhotonenTorpedo)
+						{
+							abfall.Add(spielObjekt);
+						}
+					}
+
+					PruefeKollisionen(abfall);
+
+					zeichenflaeche.Children.Clear();
+
+					foreach (SpielObjekt spielObjekt in spielObjekte)
+					{
+						spielObjekt.Zeichne(zeichenflaeche);
+					}
+				}
+				else
+				{
+					zeichenflaeche.Children.Clear();
+					raumschiff = null;
+					spielObjekte.Clear();
+					border.Background = Brushes.Red;
+					button_start.IsEnabled = true;
+					spielLaeuft = false;
+					timer.Stop();
+				}
+
+				if (zeit / vergangen >= 5d)
+				{
+					vergangen += 1;
+
+					for (int i = 0; i < (vergangen / 2) + 3; i++)
+					{
+						spielObjekte.Add(new Asteroid(zeichenflaeche, vergangen));
+					}
+
+					if (leben < 99d)
+					{
+						leben += 1;
+					}
+					else
+					{
+						leben = 100;
+					}
+
+					if (xp >= 400 && upgrades < 4)
+					{
+						upgrades++;
+						xp = 0;
+					}
+				}
+
+				if (schiessen)
+				{
+					Color color1 = Color.FromArgb(255, 0, 100, 255);
+					Color color2 = Color.FromArgb(255, 0, 255, 255);
+					Color color3 = Color.FromArgb(255, 255, 105, 180);
+					Color color4 = Color.FromArgb(255, 0, 100, 255);
+					Color color5 = Color.FromArgb(255, 0, 255, 100);
+
+					switch (upgrades)
+					{
+
+						case 0:
+							spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+							break;
+						case 1:
+							if (alternieren)
+							{
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, -3, color2));
+								alternieren = false;
+							}
+							else
+							{
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, 3, color2));
+								alternieren = true;
+							}
+							break;
+						case 2:
+							if (alternieren)
+							{
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, 6, color3));
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, -6, color3));
+								alternieren = false;
+							}
+							else
+							{
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, 3, color2));
+								spielObjekte.Add(new PhotonenTorpedo(raumschiff, -3, color2));
+								alternieren = true;
+							}
+							break;
+						case 3:
+							if (alternieren)
+							{
+								if (alternieren2)
+								{
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, -9, color4));
+								}
+								else
+								{
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 6, color3));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, -6, color3));
+								}
+								alternieren = false;
+							}
+							else
+							{
+								if (alternieren2)
+								{
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 9, color4));
+									alternieren2 = false;
+								}
+								else
+								{
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 3, color2));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, -3, color2));
+									alternieren2 = true;
+								}
+								alternieren = true;
+							}
+							break;
+						case 4:
+							if (alternieren)
+							{
+								if (alternieren2)
+								{
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, -9, color4));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 12, color5));
+								}
+								else
+								{
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 6, color3));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, -6, color3));
+								}
+								alternieren = false;
+							}
+							else
+							{
+								if (alternieren2)
+								{
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 0, color1));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 9, color4));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, -12, color5));
+									alternieren2 = false;
+								}
+								else
+								{
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, 3, color2));
+									spielObjekte.Add(new PhotonenTorpedo(raumschiff, -3, color2));
+									alternieren2 = true;
+								}
+								alternieren = true;
+							}
+							break;
+						default:
+							break;
+					}
+				}
+			}
+		}
+
+		void PruefeKollisionen(List<SpielObjekt> abfall)
+		{
+			foreach (Asteroid asteroid in spielObjekte.OfType<Asteroid>())
+			{
+				foreach (PhotonenTorpedo photonenTorpedo in spielObjekte.OfType<PhotonenTorpedo>())
+				{
+					if (asteroid.EnthaeltPunkt(photonenTorpedo.MyX, photonenTorpedo.MyY))
+					{
+						if (asteroid.Damage())
+						{
+							abfall.Add(asteroid);
+						}
+						abfall.Add(photonenTorpedo);
+						score += 2;
+						xp += 2;
+
+					}
+				}
+				foreach (Raumschiff raumschiff in spielObjekte.OfType<Raumschiff>())
+				{
+					if (asteroid.EnthaeltPunkt(raumschiff.MyX, raumschiff.MyY))
+					{
+						leben -= 33;
+						abfall.Add(asteroid);
+						if (upgrades > 1)
+						{
+							upgrades -= 2;
+						}
+						else if (upgrades > 0)
+						{
+							upgrades--;
+						}
+						raumschiff.MyX = 0.5d * zeichenflaeche.ActualWidth;
+						raumschiff.MyY = 0.5d * zeichenflaeche.ActualHeight;
+					}
+				}
+			}
+			foreach (SpielObjekt spielObjekt in abfall)
+			{
+				spielObjekte.Remove(spielObjekt);
+			}
+		}
+
+		void Auserhalb()
+		{
+			List<SpielObjekt> auserhalb = new List<SpielObjekt>();
+
+			foreach (PhotonenTorpedo photonenTorpedo in spielObjekte.OfType<PhotonenTorpedo>())
+			{
+				if (photonenTorpedo.MyX < 0d)
+				{
+					auserhalb.Add(photonenTorpedo);
+				}
+				else if (photonenTorpedo.MyX > zeichenflaeche.ActualWidth)
+				{
+					auserhalb.Add(photonenTorpedo);
+				}
+
+				if (!auserhalb.Contains(photonenTorpedo))
+				{
+					if (photonenTorpedo.MyY < 0d)
+					{
+						auserhalb.Add(photonenTorpedo);
+					}
+					else if (photonenTorpedo.MyY > zeichenflaeche.ActualHeight)
+					{
+						auserhalb.Add(photonenTorpedo);
+					}
+				}
+			}
+			foreach (SpielObjekt spielObjekt in auserhalb)
+			{
+				spielObjekte.Remove(spielObjekt);
+			}
+		}
+
+		private void Button_start_Click(object sender, RoutedEventArgs e)
+		{
+			spielLaeuft = true;
+			button_start.IsEnabled = false;
+			zeit = 0;
+			leben = 100;
+			score = 0;
+			vergangen = 1;
+			schiessen = false;
+			upgrades = 0;
+			xp = 0;
+			prograssBar_health.Value = 100;
+			textBlock_health.Text = "100";
+			textBlock_score.Text = "000";
+			border.Background = Brushes.Black;
+			raumschiff = new Raumschiff(zeichenflaeche);
+			spielObjekte.Add(raumschiff);
+			timer.Start();
+
+			for (int i = 0; i < 10; i++)
+			{
+				spielObjekte.Add(new Asteroid(zeichenflaeche, vergangen));
+			}
+		}
+
+		private void Window_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (spielLaeuft == true)
+			{
+				switch (e.Key)
+				{
+					case Key.W:
+						raumschiff.MyYvel = -400d;
+						break;
+					case Key.S:
+						raumschiff.MyYvel = 400d;
+						break;
+					case Key.A:
+						raumschiff.MyXvel = -400d;
+						break;
+					case Key.D:
+						raumschiff.MyXvel = 400d;
+						break;
+					case Key.F:
+						if (upgrades < 4)
+						{
+							upgrades += 1;
+						}
+						break;
+					default:
+						break;
+				}
+			}
+			else if (e.Key == Key.Space)
+			{
+				button_start.RaiseEvent(new RoutedEventArgs(ButtonBase.ClickEvent));
+			}
+		}
+
+		private void Window_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (spielLaeuft == true)
+			{
+				switch (e.Key)
+				{
+					case Key.W:
+						raumschiff.MyYvel = 0d;
+						break;
+					case Key.S:
+						raumschiff.MyYvel = 0d;
+						break;
+					case Key.A:
+						raumschiff.MyXvel = 0d;
+						break;
+					case Key.D:
+						raumschiff.MyXvel = 0d;
+						break;
+					default:
+						break;
+				}
+			}
+		}
+
+		private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+		{
+			schiessen = true;
+		}
+
+		private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+		{
+			schiessen = false;
+		}
+	}
+}
---
+++ Spiel/SpielObjekt.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Spiel
+{
+	abstract class SpielObjekt
+	{
+		double x;
+		double y;
+		double xVel;
+		double yVel;
+		public double MyX { get => x; set => x = value; }
+		public double MyY { get => y; set => y = value; }
+		public double MyXvel { get => xVel; set => xVel = value; }
+		public double MyYvel { get => yVel; set => yVel = value; }
+
+
+		protected SpielObjekt(double x, double y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+
+		protected SpielObjekt(double x, double y, double vx, double vy)
+		{
+			this.x = x;
+			this.y = y;
+			this.xVel = vx;
+			this.yVel = vy;
+		}
+
+		public bool Animiere(Canvas zeichenflaeche, TimeSpan intervall)
+		{
+			x += MyXvel * intervall.TotalSeconds;
+			y += MyYvel * intervall.TotalSeconds;
+
+			bool uebertretung = false;
+
+			if (x < 0d)
+			{
+				x = zeichenflaeche.ActualWidth;
+				uebertretung = true;
+			}
+			else if (x > zeichenflaeche.ActualWidth)
+			{
+				x = 0d;
+				uebertretung = true;
+			}
+
+			if (y < 0d)
+			{
+				y = zeichenflaeche.ActualHeight;
+				uebertretung = true;
+			}
+			else if (y > zeichenflaeche.ActualHeight)
+			{
+				y = 0d;
+				uebertretung = true;
+			}
+
+			return uebertretung;
+		}
+
+		public abstract void Zeichne(Canvas zeichenflaeche);
+	}
+
+	class Asteroid : SpielObjekt
+	{
+		static Random zufall = new Random();
+		Polygon umriss = new Polygon();
+		int leben = 10;
+
+		public Asteroid(Canvas zeichenflaeche, int multiplier)
+			: base(zufall.NextDouble() * zeichenflaeche.ActualWidth, zufall.NextDouble() * zeichenflaeche.ActualHeight,
+					(zufall.NextDouble() - 0.5) * 400d, (zufall.NextDouble() - 0.5) * 400d)
+		{
+			if (MyX - zeichenflaeche.ActualWidth < MyX)
+			{
+				if ((MyX - zeichenflaeche.ActualWidth) / 1.6 < MyY - zeichenflaeche.ActualHeight && (MyX - zeichenflaeche.ActualWidth) / 1.6 < MyY)
+				{
+					MyX = zeichenflaeche.ActualWidth;
+				}
+				else
+				{
+					if (MyY - zeichenflaeche.ActualHeight < MyY)
+					{
+						MyY = zeichenflaeche.ActualHeight;
+					}
+					else
+					{
+						MyY = 0d;
+					}
+				}
+
+			}
+			else
+			{
+				if (MyX / 1.6 < MyY - zeichenflaeche.ActualHeight && MyX / 1.6 < MyY)
+				{
+					MyX = 0d;
+				}
+				else
+				{
+					if (MyY - zeichenflaeche.ActualHeight < MyY)
+					{
+						MyY = zeichenflaeche.ActualHeight;
+					}
+					else
+					{
+						MyY = 0d;
+					}
+				}
+			}
+
+
+			for (int i = 0; i < 20; i++)
+			{
+				double alpha = 2d * Math.PI / 20d * i;
+				double radius = 24d + 12d * zufall.NextDouble();
+				umriss.Points.Add(new Point(radius * Math.Cos(alpha), radius * Math.Sin(alpha)));
+			}
+			umriss.Fill = Brushes.Gray;
+
+			leben = 10 + multiplier / 4;
+		}
+
+		public override void Zeichne(Canvas zeichenflaeche)
+		{
+			zeichenflaeche.Children.Add(umriss);
+			Canvas.SetLeft(umriss, MyX);
+			Canvas.SetTop(umriss, MyY);
+		}
+
+		public bool EnthaeltPunkt(double x, double y)
+		{
+			return umriss.RenderedGeometry.FillContains(new Point(x - MyX, y - MyY));
+		}
+
+		public bool Damage()
+		{
+			leben -= 1;
+			return leben <= 0;
+		}
+	}
+
+	class Raumschiff : SpielObjekt
+	{
+		Polygon umriss = new Polygon();
+		RotateTransform rotation = null;
+		public RotateTransform Rotation { get => rotation; }
+
+		public Raumschiff(Canvas zeichenflaeche)
+			: base(0.5d * zeichenflaeche.ActualWidth, 0.5d * zeichenflaeche.ActualHeight)
+		{
+			umriss.Points.Add(new Point(0d, -15d));
+			umriss.Points.Add(new Point(10d, 10d));
+			umriss.Points.Add(new Point(-10d, 10d));
+			umriss.Fill = Brushes.Red;
+		}
+
+		public override void Zeichne(Canvas zeichenflaeche)
+		{
+			zeichenflaeche.Children.Add(umriss);
+			Canvas.SetLeft(umriss, MyX);
+			Canvas.SetTop(umriss, MyY);
+		}
+
+		public void Rotieren(Canvas zeichenflaeche, Point mausPos)
+		{
+			Point schiffPos = zeichenflaeche.PointToScreen(new Point(this.MyX, this.MyY));
+			double xDiff = mausPos.X - 210d - schiffPos.X;
+			double yDiff = mausPos.Y - 30d - schiffPos.Y;
+			rotation = new RotateTransform(90 + (Math.Atan2(yDiff, xDiff) * 180 / Math.PI));
+			umriss.RenderTransform = rotation;
+		}
+	}
+
+	class PhotonenTorpedo : SpielObjekt
+	{
+		Ellipse umriss = new Ellipse();
+
+		public PhotonenTorpedo(Raumschiff raumschiff, int abweichung, Color color)
+			: base(raumschiff.MyX, raumschiff.MyY,
+					Math.Cos((-90 + abweichung + raumschiff.Rotation.Angle) * Math.PI / 180) * 1500d, Math.Sin((-90 + abweichung + raumschiff.Rotation.Angle) * Math.PI / 180) * 1500d)
+		{
+			umriss.Width = 3d;
+			umriss.Height = 10d;
+			umriss.Fill = new SolidColorBrush(color);
+			umriss.RenderTransform = raumschiff.Rotation;
+		}
+
+		public override void Zeichne(Canvas zeichenflaeche)
+		{
+			zeichenflaeche.Children.Add(umriss);
+			Canvas.SetLeft(umriss, MyX);
+			Canvas.SetTop(umriss, MyY);
+		}
+	}
+}
